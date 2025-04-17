dd6b999722ffe79ae4165dbf98eab731dbbcd459
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2025-04-16 23:47:19 -0300

    Create App.js

---
+++ Frontend/Public/Src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Frontend</h1>
+      <p>Este é um exemplo básico de frontend usando React.</p>
+    </div>
+  );
+}
+
+export default App;
